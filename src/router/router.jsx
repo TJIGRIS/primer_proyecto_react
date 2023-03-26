03d753ced7c60b046d5353cfb8ac5976e098831f
JAlvarezz18 <johanalvarezz237@gmail.com>
2023-03-25 21:04:04 -0500

    gfg

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from '../App';
 
 export const router = () => {
   return (
-    <div>router</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   )
 }
