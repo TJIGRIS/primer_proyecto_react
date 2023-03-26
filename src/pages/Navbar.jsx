80c6cfeab4258ff551c6d52b7ed1d8179798d7f4
JAlvarezz18 <johanalvarezz237@gmail.com>
2023-03-25 23:44:23 -0500

    holi

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../components/navbar/Header";
+import { Nav } from "../components/navbar/Nav";
 
 export const Navbar = () => {
   return (
-    <div className="text-3xl font-bold underline">Navbar</div>
-  )
-}
+    <div className="">
+      <Nav />
+      <Header />
+    </div>
+  );
+};
