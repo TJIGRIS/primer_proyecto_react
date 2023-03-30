c1477fe036959e296415d59c0fd090fe4be26b88
JAlvarezz18 <johanalvarezz237@gmail.com>
2023-03-30 13:15:27 -0500

    swiper2

--- src/components/hero/NewItems.jsx
+++ src/components/hero/NewItems.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { fox } from "../../assets/index";
+import {fox} from '../../assets/index';
 
-export const HeroEmpresas = () => {
+export const NewItems = () => {
   return (
     <article className="bg-[#282b30] grid place-items-center p-6 rounded-3xl h-[12rem] box-shadow2">
-      <div>
-        <img src={fox} alt="" />
-      </div>
-      <p>Merasmak</p>
+      
     </article>
   );
 };
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { HeroEmpresas } from '../components/hero/HeroEmpresas'
+import React from "react";
+import { HeroEmpresas } from "../components/hero/HeroEmpresas";
+import { NewItems } from "../components/hero/NewItems";
+
 
 export const Hero = () => {
   return (
-    <section className="container mx-auto">
-      {/* heroEmpresas  */}
-      <HeroEmpresas />
+    <section className="box-shadow">
+      <div className="container mx-auto">
+        {/* heroEmpresas  */}
+        <div className="grid grid-cols-6 place-content-between gap-8">
+          <HeroEmpresas />
+        </div>
 
-
-
+        {/* NewItems  */}
+        <div className="">
+          
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
