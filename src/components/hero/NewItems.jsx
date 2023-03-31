01f347f16d6eb51e1ce36fd257d0e37c1b898b18
JAlvarezz18 <johanalvarezz237@gmail.com>
2023-03-31 10:02:18 -0500

    switer

--- src/components/hero/NewItems.jsx
+++ src/components/hero/NewItems.jsx
@@ -4,7 +4,7 @@ import {fox} from '../../assets/index';
 export const NewItems = () => {
   return (
     <article className="bg-[#282b30] grid place-items-center p-6 rounded-3xl h-[12rem] box-shadow2">
-      
+      1
     </article>
   );
 };
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { HeroEmpresas } from "../components/hero/HeroEmpresas";
 import { NewItems } from "../components/hero/NewItems";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import '../styles.css'
+
+// import required modules
+import { Navigation } from "swiper";
 
 export const Hero = () => {
   return (
@@ -14,7 +24,24 @@ export const Hero = () => {
 
         {/* NewItems  */}
         <div className="">
-          
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            rewind={true}
+            navigation={true}
+            modules={[Navigation]}
+            className="mySwiper"
+          >
+            
+              <SwiperSlide>1</SwiperSlide>
+            
+              <SwiperSlide>1</SwiperSlide>
+            
+              <SwiperSlide>1</SwiperSlide>
+            
+              <SwiperSlide>1</SwiperSlide>
+            
+          </Swiper>
         </div>
       </div>
     </section>
