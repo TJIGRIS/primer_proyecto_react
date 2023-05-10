238961b05c5dc433bf1a748c812acbc3675b1234
TJIGRIS <johanalvarezz237@gmail.com>
2023-05-10 15:55:42 -0500

    segundo intento

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://TJIGRIS.github.io/primer_proyecto_react",
+  base: "vite/deploy",
 });
